5cbcb813c5d7369e928afe5b82d16a04d6a0ab1e
dtesfu789 <ddytesfu@gmail.com>
2022-05-12 21:46:41 +0200

    simple webapp running

--- calculator.js
+++ calculator.js
@@ -1,9 +1,30 @@
 const express = require('express');
+const bodyParser = require("body-parser");
 const app =express();
 const port =3000;
+
+app.use(bodyParser.urlencoded({extended:true}));
 app.get("/",function (req,res) {
-  res.send("<h1>Hello</h1>");
+  res.sendFile(__dirname+"/index.html");
+});
+app.post("/",function (req,res) {
+  var num1 =Number(req.body.num1);
+  var num2 = Number(req.body.num2);
+  var result = num1+num2;
+
+  res.send("result is "+result);
 });
 app.listen(port,function () {
   console.log("server started on 3000");
 });
+
+
+app.get("/bmicalculator",function (req,res) {
+  res.sendFile(__dirname+"/bmicalculator.html");
+});
+app.post("/bmicalculator",function (req,res) {
+var weight = parseFloat(req.body.weight);
+var height = parseFloat(req.body.height);
+var result = weight/Math.pow(height,2);
+res.send("BMI is "+result);
+})
